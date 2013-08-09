d5953e789e1ee030b3878e2bc5d97f2a23ad7179
Pairing Station <modcloth@nibbler-dev-sfo.modcloth.int>
2013-08-09 15:00:02 -0700

    First successfully converged commit on nibbler-dev-sfo

--- site-cookbooks/meta/recipes/osx_apps.rb
+++ site-cookbooks/meta/recipes/osx_apps.rb
@@ -1,10 +1,7 @@
 include_recipe "sprout-osx-apps::ack"
 include_recipe "sprout-osx-apps::caffeine"
-include_recipe "sprout-osx-apps::evernote"
 include_recipe "sprout-osx-apps::flux"
 include_recipe "sprout-osx-apps::freeruler"
-include_recipe "sprout-osx-apps::things"
-include_recipe "sprout-osx-apps::1password"
 include_recipe "pivotal_workstation::omnigraffle"
 
 

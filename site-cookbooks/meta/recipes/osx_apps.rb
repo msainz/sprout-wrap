69bc5af9100d889545cf233d2dc99d6f5c0f8a7a
Marcos Sainz <marcos.sainz+marcos@glassdoor.com>
2013-09-10 07:57:47 -0700

    Initial convergence on gldr-msainz-laptop

--- site-cookbooks/meta/recipes/osx_apps.rb
+++ site-cookbooks/meta/recipes/osx_apps.rb
@@ -3,7 +3,6 @@ include_recipe "sprout-osx-apps::caffeine"
 include_recipe "sprout-osx-apps::evernote"
 include_recipe "sprout-osx-apps::flux"
 include_recipe "sprout-osx-apps::freeruler"
-include_recipe "sprout-osx-apps::things"
 include_recipe "pivotal_workstation::omnigraffle"
 
 
